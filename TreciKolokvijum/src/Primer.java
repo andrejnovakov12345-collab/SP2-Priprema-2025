150ac8043eb512cfaf919bfc7a49797cd2a41802
Andrej Novakov <andrejnovakov12345@gmail.com>
2025-08-21 13:38:50 +0200

    Obrisan komentar pre oddOrPos

--- TreciKolokvijum/src/Primer.java
+++ TreciKolokvijum/src/Primer.java
@@ -1,20 +1,11 @@
 
 
 public class Primer {
-	/**
-	 * Count odd or postive elements
-	 *
-	 * @param x
-	 *            array to search
-	 * @return count of odd/positive values in x
-	 * @throws NullPointerException
-	 *             if x is null
-	 */
 	public int oddOrPos(int[] x) {
 		int count = 0;
 		for (int i = 0; i < x.length - 1; i++) {
 			if (x[i] % 2 == 1 && x[i] > 0) {
-				count++;
+				x.length + 1;
 			}
 		}
 		return count;
